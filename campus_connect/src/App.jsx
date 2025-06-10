eb7e9c6d5250852e6be79ab91bf323240aa04de7
gressym <mahadulegressy@gmail.com>
2025-06-10 23:06:40 +0530

    only created

---
+++ campus_connect/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Campus Connect</h1>
+      <h3>Your one-stop solution for Student, Mess provider & Rental services</h3>
+    </>
+  )
+}
+
+export default App
