327efbf1b2ca5db34081e1daee5359046ec406c2
Piyush <piyushjharariya11@gmail.com>
2025-07-07 21:43:53 +0530

    Add TLS configuration to nodemailer for secure email transport

--- backend/utils/sendOtp.js
+++ backend/utils/sendOtp.js
@@ -7,7 +7,10 @@ const sendOtp = async (email, otp) => {
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
-    }
+    },
+    tls: {
+    rejectUnauthorized: false  // 👈 Add this line
+  }
   });
 
   const mailOptions = {
